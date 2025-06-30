1c127923d7f70e376ec7207d234911deacb0c052
Muyz <muyz0319@icloud.com>
2025-06-30 11:45:22 +0800

    Initial Commit

---
+++ DungeonStat/DungeonStatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonStatApp.swift
+//  DungeonStat
+//
+//  Created by 黄天晨 on 2025/6/30.
+//
+
+import SwiftUI
+
+@main
+struct DungeonStatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
